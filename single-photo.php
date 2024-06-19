0034ee7ea70e8b3a3d8975ba5a7532578dfd9887
Ben Viseo <benjamin@expert-viseo.com>
2024-06-19 12:49:05 +0200

    Ajout bloc cta single photo header

--- single-photo.php
+++ single-photo.php
@@ -5,13 +5,48 @@ if ( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
 
 <div class="header-photo">
-    <div class="column-left">
-        <h1 class="title-single-photo"><?php the_title(); ?></h1>
-        <p><?php echo get_field('reference'); ?></p>
-        <p><?php echo get_field('type'); ?></p>
+    <div class="row-photo">
+        <div class="column-left">
+            <h1 class="title-single-photo"><?php the_title(); ?></h1>
+            <p><?php echo get_field('reference'); ?></p>
+            <p><?php echo get_field('type'); ?></p>
+            <?php $post_terms = get_the_terms( get_the_ID(), 'categoriesphotos' ); ?>
+            <p>
+            <?php 
+            $post_terms = get_the_terms( get_the_ID(), 'format' ); 
+            echo $post_terms[0]->name;
+            ?>
+            </p>
+            <p>
+            <?php 
+            $post_terms = get_the_terms( get_the_ID(), 'categoriesphotos' ); 
+            echo $post_terms[0]->name;
+            ?>
+            </p>
+            <p><?php the_time('Y'); ?></p>
+
+        </div>
+        <div class="column-right">
+            <?php the_post_thumbnail();?>   
+        </div>
     </div>
-    <div class="column-right">
-        <?php the_post_thumbnail();?>   
+    <div class="row-cta">
+        <div class="column-left">
+            <?php 
+            $question_single = get_field('question_single', 'option');
+            if( !empty( $question_single ) ): 
+            ?>
+            <p><?php echo $question_single; ?></p>
+            <?php endif; ?>
+            <?php 
+            $bouton_single = get_field('bouton_contact_single', 'option');
+            if( !empty( $bouton_single ) ): 
+            ?>
+            <button class="bouton-single" type="button"><?php echo $bouton_single; ?></button>         
+        <?php endif; ?>
+        </div>
+        <div class="column-right">
+        </div>
     </div>
 </div>
         <div><?php the_content(); ?></div>
