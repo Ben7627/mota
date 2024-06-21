8c479007f7928ee2ff37aa6d9225299791596cff
Ben Viseo <benjamin@expert-viseo.com>
2024-06-21 15:38:08 +0200

    Ajout responsive single photo et js bouton contact pour modale

--- assets/js/script-contact.js
+++ assets/js/script-contact.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Sélectionner le dernier élément du menu
     let lastMenuItem = document.querySelector('#menu-menu-principal .menu-item:last-child');
+    let boutonContact = document.querySelector('.bouton-single');
 
     // Ajouter un gestionnaire d'événement pour le clic sur le dernier élément du menu
     if (lastMenuItem) {
@@ -20,7 +21,23 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+    if (boutonContact) {
+        boutonContact.addEventListener('click', function(event) {
+            event.preventDefault(); // Empêche l'action par défaut du lien
 
+            // Sélectionner la div avec la classe popup
+            let popup = document.querySelector('.popup');
+            let popupfond = document.querySelector('.fond-popup');
+
+            // Ajouter la classe open à la div popup et à la div fond-popup
+            if (popup) {
+                popup.classList.add('open');
+            }
+            if (popupfond) {
+                popupfond.classList.add('open-fond');
+            }
+        });
+    }
     // Ajouter un gestionnaire d'événement pour fermer la popup en cliquant sur fond-popup.open-fond
     document.addEventListener('click', function(event) {
         // Sélectionner la div avec la classe fond-popup et open-fond
