3185cf835b4e4502d4df76ce273b9c63fab26752
Ben Viseo <benjamin@expert-viseo.com>
2024-06-26 10:50:10 +0200

    Ajout fermeture menu burger mobile

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -1,14 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     let buttonburger = document.querySelector('.button-nav-menu-mobile');
+    let buttonClose = document.querySelector('.button-close-menu-mobile');
 
     if (buttonburger) {
-    buttonburger.addEventListener('click', function(event) {
-    let menu = document.querySelector('.main-navigation');
-    let button = document.querySelector('.js-menu-toggle.menu-toggle')
-    console.log(buttonburger)
+        buttonburger.addEventListener('click', function(event) {
+            let menu = document.querySelector('.main-navigation');
+            let button = document.querySelector('.js-menu-toggle.menu-toggle');
+            console.log(buttonburger);
 
-    menu.classList.add('open');
-    button.classList.add('active');
-    });
+            menu.classList.add('open');
+            button.classList.add('active');
+        });
+    }
+
+    if (buttonClose) {
+        buttonClose.addEventListener('click', function(event) {
+            let menu = document.querySelector('.main-navigation');
+            let button = document.querySelector('.js-menu-toggle.menu-toggle');
+            console.log(buttonClose);
+
+            menu.classList.remove('open');
+            button.classList.remove('active');
+        });
     }
 });
