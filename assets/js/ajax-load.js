6dafe7f6d038f502a0ef1df3c0f38940d35b0aae
Ben Viseo <benjamin@expert-viseo.com>
2024-07-04 11:22:43 +0200

    Finalisation du bouton load more pour qu'il disparaisse quand plus de vignettes

--- assets/js/ajax-load.js
+++ assets/js/ajax-load.js
@@ -6,16 +6,19 @@ jQuery(document).ready(function($) {
         $.ajax({
             type: 'POST',
             url: mota_js.ajaxurl,
-            dataType: 'html',
+            dataType: 'json',
             data: {
                 action: 'mota_load_more',
                 paged: currentPage,
             },
-            success: function (res) {
-                if (res.trim() === 'no_more_posts') {
+            success: function(res) {
+                if (res.html.trim() === '') {
                     $('.load-button-photos').hide();
                 } else {
-                    $('.latest-photos').append(res);
+                    $('.latest-photos').append(res.html);
+                }
+                if (!res.more_posts) {
+                    $('.load-button-photos').hide();
                 }
             }
         });
--- inc/register-ajax.php
+++ inc/register-ajax.php
@@ -1,27 +1,42 @@
 <?php 
     // Règle ajax bouton homepage
   
-  function mota_load_more() {
-    $paged = $_POST['paged'];
-    $args = array(
-        'post_type' => 'photo',
-        'posts_per_page' => 8,
-        'paged' => $paged,
-        'orderby' => 'date',
-        'order' => 'DESC',
-        'post_status' => 'publish',
-    );
-    $posts = new WP_Query($args);
+    function mota_load_more() {
+        $paged = $_POST['paged'];
+        $args = array(
+            'post_type' => 'photo',
+            'posts_per_page' => 8,
+            'paged' => $paged,
+            'orderby' => 'date',
+            'order' => 'DESC',
+            'post_status' => 'publish',
+        );
+        $posts = new WP_Query($args);
+    
+        ob_start(); // Démarrer le tampon de sortie
+    
+        if ($posts->have_posts()) {
+            while ($posts->have_posts()) {
+                $posts->the_post();
+                get_template_part('templates-part/latest-photos');
+            }
+            wp_reset_postdata();
+            $html = ob_get_clean(); // Récupérer le contenu du tampon et le nettoyer
 
-    if ($posts->have_posts()) {
-        while ($posts->have_posts()) {
-            $posts->the_post();
-            get_template_part('templates-part/latest-photos');
+            $more_posts = $posts->max_num_pages > $paged;
+            echo json_encode(array(
+                'html' => $html,
+                'more_posts' => $more_posts
+            ));
+        } else {
+            echo json_encode(array(
+                'html' => '',
+                'more_posts' => false
+            ));
         }
         wp_die();
-    } else {
-        wp_die();
     }
-}
+    
+    
 add_action('wp_ajax_mota_load_more', 'mota_load_more');
 add_action('wp_ajax_nopriv_mota_load_more', 'mota_load_more');
